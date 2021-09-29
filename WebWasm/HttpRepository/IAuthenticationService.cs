4729d953fb9744fd3c102cda3c33f9c6b52f4082
Alfonso <fonsuspro@gmail.com>
2021-09-29 18:37:33 -0500

    inicial

---
+++ WebWasm/HttpRepository/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebWasm.Dto;
+
+namespace WebWasm.HttpRepository
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDto> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<AuthResponseDto> Login(UserForAuthenticationDto userForAuthentication);
+        Task Logout();
+    }
+}
---
+++ WebWasm/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Blazored.LocalStorage;
+using WebWasm.AuthProviders;
+using Microsoft.AspNetCore.Components.Authorization;
+using WebWasm.HttpRepository;
+
+namespace WebWasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44354/api/") });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+            });
+
+
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
